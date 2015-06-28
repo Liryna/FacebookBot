1b9780e6c3c409c2294b8a692994b223b2757599
Liryna <liryna.stark@gmail.com>
2015-06-28 05:36:11 -0700

    first commit

---
+++ facebookbot.js
@@ -0,0 +1,141 @@
+var Bot = require('node-telegram-bot');
+var login = require("facebook-chat-api");
+
+if(!process.env.TELEGRAM_USER || !process.env.APP_TOKEN)
+    return console.log("Please define this env variables -- TELEGRAM_USER - APP_TOKEN");
+
+var owner = {username: process.env.TELEGRAM_USER, chat_id: ''};
+var maxThreadNb = 10;
+
+function getUsage() {
+    return "I'm a Facebook bot. I help you to communicate with your old Facebook friends!\n"
+	+ "I will forward you every of your FB messages. If reply to your FB friend messages and I will send them to your friends.\n\n"
+	+ "Available commands:\n"
+	+ "/threadList - List the last converstion you had with your friends.\n"
+	+ "/cancel - Cancel the current command\n"
+        + "\n\nMore Information: ";
+}
+
+var chat = new Array();
+var threadListTmp;
+var currentThreadId;
+
+function reset() {
+    currentThreadId = undefined;
+    threadListTmp = undefined;    
+}
+
+login("config.json", function(err, api) {
+    if(err) return console.error(err);
+
+    //listen telegram message
+    var bot = new Bot({
+	token: process.env.APP_TOKEN
+    }).on('message', function (message) {
+	if (message.from.username != owner.username)
+	    bot.sendMessage({chat_id: message.chat.id, text: "you are not my owner! Go away !"});
+	else
+	{
+	    if (!!owner.chat_id)
+		owner.chat_id = message.chat.id; //save owner chat id - TODO save in config file
+	    
+	    if (!!message.reply_to_message
+		&& !!chat[message.reply_to_message.message_id]) { //it is a reply message from FB
+
+		api.sendMessage(message.text,
+				chat[message.reply_to_message.message_id], function(err, api) {
+				    if(err) return console.error(err);
+				});
+	    } else {
+
+		if (message.text == "/threadList") {
+		    api.getThreadList(0, maxThreadNb, function callback(err, arr) {
+
+			var ft = require('./lib/findThread');
+			var fbids = ft.getParticipantsIds(arr);
+			
+			api.getUserInfo(fbids, function(err, ret) {
+			    if(err) return console.error(err);
+			    
+			    ft.createThreads(arr, ret, function(conversatioNames, newThreadListTmp) {
+				threadListTmp = newThreadListTmp;
+				bot.sendMessage({chat_id: message.chat.id,
+						 text: "Who is the recipient ?",
+						 reply_markup: {
+						     keyboard: conversatioNames
+						 }
+						}, function(err, ret) {
+						    if(err) return console.error(err);
+						})
+			    });
+			});
+		    });
+		} else if (message.text == "/cancel") {
+		    reset();
+		    bot.sendMessage({chat_id: message.chat.id,
+				     text: "Command canceled.",
+				     reply_markup: {
+					 hide_keyboard: true
+				     }
+				    },
+				    function(err, ret) {
+					if(err) return console.error(err);
+				    });
+		} else if (currentThreadId != undefined) { //send the message to the current recipient
+		    api.sendMessage(message.text,
+				    currentThreadId, function(err, api) {
+					if(err) return console.error(err);
+				    });
+		} else if (threadListTmp != undefined) { //Check if owner have send a good recipient name
+		    currentThreadId = undefined;
+		    for (var x = 0; x < threadListTmp.length; x++) {
+			if (threadListTmp[x].name == message.text)
+			    currentThreadId = threadListTmp[x].thread_id;
+		    }
+		    
+		    if (currentThreadId != undefined)
+			bot.sendMessage({chat_id: message.chat.id,
+					 text: "What is the message for him ?",
+					 reply_markup: {
+					     hide_keyboard: true
+					 }
+					},
+					function(err, ret) {
+					    if(err) return console.error(err);
+					});
+		    else
+			bot.sendMessage({chat_id: message.chat.id,
+					 text: "I do not know him, Please give me a correct name or /cancel."},
+					function(err, ret) {
+					    if(err) return console.error(err);
+					});
+		} else {
+		    bot.sendMessage({chat_id: message.chat.id,
+				     text: getUsage()},
+				    function(err, ret) {
+					if(err) return console.error(err);
+				    });
+		}
+	    }
+	}
+    }).start();
+        
+    
+    //listen message from FB and forward to telegram
+    api.listen(function callback(err, message) {
+	
+	console.log(message);
+	
+	var forwardmsg = message.sender_name + ": " + message.body;
+	if (!!owner.chat_id)
+            bot.sendMessage({chat_id: owner.chat_id, text: forwardmsg}, function(err, res) {
+		if(err) return console.error(err);
+		
+		//save message id send and fb thread id for futur reply
+		chat[res.message_id] = message.thread_id;
+	    })
+	else
+	    console.log("where are you my owner ?");	
+    });
+});
+
